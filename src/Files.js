a7dac7a19378bb7b09c8838f73810d3a358e4ced
CornWorld <cw@had.ink>
2022-03-30 19:16:48 +0800

    [Feat] Add support for `FileTag`

--- src/Files.js
+++ src/Files.js
@@ -1,105 +1,131 @@
 import React from 'react';
 
-import {Container, Divider, Grid, Header, Icon, Input, List, Menu, Segment} from "semantic-ui-react";
+import {Divider, Grid, Header, Icon, Input, Label, Menu, Popup, Segment, Table} from "semantic-ui-react";
 
-import fileList from './data/fileList.json';
-
-
-function fileIconName(type) {
-    switch (type) {
-        default:
-            return 'file';
-    }
-}
-
+import files from './data/files.json';
+import {Link} from "react-router-dom";
 
 export default function Files() {
     return (
         <div id='body'>
-            <Header size='large'>
-                <Icon name={'battery full'}/>
-                <Header.Content>
-                    每周良心软件推荐
-                    <Header.Subheader>
-                        保证不凉心!
-                    </Header.Subheader>
-                </Header.Content>
-            </Header>
-            
-            <Divider hidden/>
-            
-            // TODO
-            <Grid stackable columns={3}>
-                <Grid.Column>
-                    <Header>
-                        <Icon name={fileIconName()}/>
-                        <Header.Content>
-                            ???
-                            <Header.Subheader>
-                                ???
-                            </Header.Subheader>
-                        </Header.Content>
-                    </Header>
-                </Grid.Column>
-                <Grid.Column>
-                    <Header>
-                        <Icon name={fileIconName()}/>
-                        <Header.Content>
-                            ???
-                            <Header.Subheader>
-                                ???
-                            </Header.Subheader>
-                        </Header.Content>
-                    </Header>
-                </Grid.Column>
-                <Grid.Column>
-                    <Header>
-                        <Icon name={fileIconName()}/>
-                        <Header.Content>
-                            ???
-                            <Header.Subheader>
-                                ???
-                            </Header.Subheader>
-                        </Header.Content>
-                    </Header>
-                </Grid.Column>
-            </Grid>
-            
-            <Divider hidden/>
+            <Segment>
+    
+                <Header size='large'>
+                    <Icon name={'battery full'}/>
+                    <Header.Content>
+                        每周良心软件推荐
+                        <Header.Subheader>
+                            保证不凉心!
+                        </Header.Subheader>
+                    </Header.Content>
+                </Header>
+                
+                <Divider hidden/>
+    
+                <Grid stackable columns={3}>
+                    <Grid.Column>
+                        <Segment>
+                            <Header>
+                                <Header.Content>
+                                    ???
+                                    <Header.Subheader>
+                                        ???
+                                    </Header.Subheader>
+                                </Header.Content>
+                            </Header>
+                        </Segment>
+                    </Grid.Column>
+                </Grid>
+            </Segment>
             
             <Menu stackable tabular attached='top'>
                 <Menu.Menu position='right'>
                     <Menu.Item>
-                        <Input
-                            transparent
-                            icon='search'
-                            placeholder='Search...'
-                        />
+                    
                     </Menu.Item>
                 </Menu.Menu>
             </Menu>
-            <Segment attached='bottom'>
-                <Container>
-                    <List relaxed>
-                        {
-                            fileList.map((item, index) => {
+            <Table attached='bottom' singleLine unstackable selectable>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Filename</Table.HeaderCell>
+                        <Table.HeaderCell>Tags</Table.HeaderCell>
+                        <Table.HeaderCell textAlign='right'>
+                            <Input
+                                transparent
+                                icon='search'
+                                placeholder='Search...'
+                            />
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {
+                        files.map((item, index) => {
+                            if (item.urlType === 'directly') {
                                 return (
-                                    <List.Item as='a' href={item.url} key={'file_' + index}>
-                                        <List.Icon name={fileIconName(item.type)} verticalAlign='middle'/>
-                                        <List.Content>
-                                            <List.Header>{item.name}</List.Header>
-                                            <List.Description>
-                                                sha1: {item.sha1}
-                                            </List.Description>
-                                        </List.Content>
-                                    
-                                    </List.Item>
+                                    <Table.Row key={'fileListItem' + index}>
+                                        <Table.Cell singleLine width={5}>
+                                            <Icon name='text file'/>
+                                            <Link to={item.url}>{item.filename}</Link>
+                                        </Table.Cell>
+                                        <Table.Cell colSpan={2}>
+                                            <Label.Group>
+                                                {
+                                                    (Object.keys(item.tags)).map((tag) => {
+                                                        switch (tag) {
+                                                            case 'hash':
+                                                                return (
+                                                                    <Popup
+                                                                        content={
+                                                                            Object.keys(item.tags[tag]).map((hash)=>{
+                                                                                return <p>{hash}: {item.tags[tag][hash]}</p>
+                                                                            })
+                                                                        }
+                                                                        on='click'
+                                                                        pinned
+                                                                        trigger={
+                                                                            <Label as='a'>
+                                                                                hash(click)
+                                                                            </Label>
+                                                                        }
+                                                                    />
+                                                                );
+                                                            default:
+                                                                return (<Label>{tag}: {item.tags[tag]}</Label>);
+                                                        }
+                                                    })
+                                                }
+                                            </Label.Group>
+                                        </Table.Cell>
+                                    </Table.Row>
                                 );
-                            })
-                        }
-                    </List>
-                </Container>
-            </Segment>
+                            }
+                        })
+                    }
+                </Table.Body>
+            </Table>
+            {/*<Segment attached='bottom'>*/}
+            {/*    <Container>*/}
+            {/*        <List relaxed>*/}
+            {/*            {*/}
+            {/*                fileList.map((item, index) => {*/}
+            {/*                    return (*/}
+            {/*                        <List.Item as='a' href={item.url} key={'file_' + index}>*/}
+            {/*                            <List.Content>*/}
+            {/*                                <List.Header>{item.name}</List.Header>*/}
+            {/*                                <List.Description>*/}
+            {/*                                    sha1: {item.sha1}*/}
+            {/*                                </List.Description>*/}
+            {/*                            </List.Content>*/}
+            {/*                        */}
+            {/*                        </List.Item>*/}
+            {/*                    );*/}
+            {/*                })*/}
+            {/*            }*/}
+            {/*        </List>*/}
+            {/*    </Container>*/}
+            {/*</Segment>*/}
         
         </div>
     
