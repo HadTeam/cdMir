28df8b5b19f513e97c10a076f7091fcd4af9e82f
CornWorld <cw@had.ink>
2022-04-11 18:24:41 +0800

    [Feat] Add search input in `Files` page

--- src/Files.js
+++ src/Files.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {Icon, Label, Pagination, Popup, Table} from 'semantic-ui-react';
+import {Icon, Label, Pagination, Popup, Search, Table} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import * as _ from 'lodash';
 
 import files from './data/processed/files.json';
 
@@ -29,15 +30,73 @@ HashPopup.propTypes = {
 
 export default function Files() {
     const [activePage, setActivePage] = React.useState(1);
-    const pageLength=10;
+    const pageLength = 10;
     
-    const shownFiles = files.filter((item) => {
+    const shownFiles=files.filter((item) => {
         return item.urlType === 'directly';
-    })
+    });
     const filesTot = shownFiles.length;
-    const pagesTot=Math.floor(filesTot / pageLength)+(filesTot % pageLength!==0);
-    console.log(pagesTot);
-    const leftNum=(activePage-1)*pageLength;
+    const pagesTot = Math.floor(filesTot / pageLength) + (filesTot % pageLength !== 0)
+    const leftNum = (activePage - 1) * pageLength;
+    
+    shownFiles.forEach((item, index)=>{
+        item.index=index;
+    });
+    
+    const searchInitState = {
+        loading: false,
+        searchResults: [],
+        value: ''
+    };
+    const searchReducer = (state, action) => {
+        switch (action.type) {
+            case 'SEARCH_START':
+                return {...state, loading: true, value: action.query};
+            case 'SEARCH_FINISH':
+                return {...state, loading: false, searchResults: action.results};
+            case 'SEARCH_UPDATE_SELECTION': {
+                setActivePage(action.selection.page);
+                return {...state, value: action.selection.title};
+            }
+            default:
+            case 'SEARCH_CLEAN':
+                return searchInitState;
+        }
+    };
+    const [searchState, searchDispatch] = React.useReducer(searchReducer, searchInitState);
+    const {loading, searchResults, value} = searchState;
+    
+    const timeoutRef = React.useRef();
+    const search = React.useCallback((e, data) => {
+        clearTimeout(timeoutRef.current)
+        searchDispatch({type: 'SEARCH_START', query: data.value})
+        
+        timeoutRef.current = setTimeout(() => {
+            if (data.value.length === 0) {
+                searchDispatch({type: 'SEARCH_CLEAN'})
+                return;
+            }
+            
+            const isMatch = (item) => {
+                let re = new RegExp(_.escapeRegExp(data.value), 'i');
+                return re.test(item.filename);
+            }
+            searchDispatch({
+                type: 'SEARCH_FINISH',
+                results: shownFiles.filter(isMatch).map((item) => {
+                    return {
+                        title: item.filename,
+                        description: item.tags.source,
+                        id: item.filename+item.tags.source,
+                        page: Math.floor(item.index/pageLength)+1
+                    };
+                })
+            });
+        }, 300);
+    }, []);
+    React.useEffect(() => {
+        clearTimeout(timeoutRef.current)
+    }, [])
     
     return (
         <div id='body'>
@@ -45,26 +104,34 @@ export default function Files() {
                 <Table.Header>
                     <Table.Row>
                         <Table.HeaderCell>Filename</Table.HeaderCell>
-                        <Table.HeaderCell textAlign='right'>Tags</Table.HeaderCell>
-                        <Table.HeaderCell textAlign='right'>
-                            {/*
-                                <Input
-                                    transparent
-                                    icon='search'
-                                    placeholder='Search...'
-                                />
-                            */}
+                        <Table.HeaderCell>
+                            <Search
+                                icon='search'
+                                placeholder='Search...'
+                                loading={loading}
+                                onSearchChange={search}
+                                results={searchResults}
+                                value={value}
+                                onResultSelect={(e, data)=>{
+                                    searchDispatch({type: 'SEARCH_UPDATE_SELECTION', selection: data.result})
+                                }}
+                            />
                         </Table.HeaderCell>
+                        <Table.HeaderCell>Tags</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
                     {
-                        shownFiles.slice(leftNum,leftNum+pageLength).map((item, index) => {
+                        shownFiles.slice(leftNum, leftNum + pageLength).map((item, index) => {
                             return (
-                                <Table.Row key={'fileListItem' + index}>
+                                <Table.Row key={index}>
                                     <Table.Cell singleLine width={5}>
                                         <Icon name='text file'/>
-                                        <a href={item.url} target='_blank' rel="noreferrer">{item.filename}</a>
+                                        <a href={item.url}
+                                           target='_blank'
+                                           rel="noreferrer"
+                                           name={item.url+item.tags.source}
+                                        >{item.filename}</a>
                                     </Table.Cell>
                                     <Table.Cell colSpan={2} textAlign='right'>
                                         <Label.Group>
@@ -97,7 +164,9 @@ export default function Files() {
                             <Pagination
                                 totalPages={pagesTot}
                                 activePage={activePage}
-                                onPageChange={(e,data)=>{setActivePage(data.activePage)}}
+                                onPageChange={(e, data) => {
+                                    setActivePage(data.activePage)
+                                }}
                             />
                         </Table.Cell>
                     </Table.Row>
