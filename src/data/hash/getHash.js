d4e3a4f231554bb446e58d6cab4e4546c882c27b
CornWorld <cw@had.ink>
2022-04-21 11:06:57 +0800

    [Feat] Complete argument parser for gen hash

--- src/data/hash/getHash.js
+++ src/data/hash/getHash.js
@@ -1,5 +1,8 @@
 import {createHash} from "crypto";
 import fs from "fs";
+import * as commander from "commander";
+
+const program = new commander.Command();
 
 function getHashFromFile(algorithm, path) {
     return createHash(algorithm).update(fs.readFileSync(path)).digest('hex');
@@ -19,29 +22,74 @@ function getCommonHashFromFile(path) {
     else return {};
 }
 
-function getFileList(path) {
-    if(path.substr(-1)!=='/') path+='/';
-    return fs.readdirSync(path).filter((item)=>{
-        return fs.statSync(path + item).isFile();
+function getFileListFromDir(dirPath) {
+    if(dirPath.substr(-1)!=='/') dirPath+='/';
+    return fs.readdirSync(dirPath).filter((filename)=>{
+        return fs.statSync(dirPath + filename).isFile();
+    }).map((filename)=>{
+        return {path: dirPath, name: filename};
     });
 }
 
-function getFilesHashFromDir(dirPath, processStartedCallback=(fileList)=>{ }, processingCallback=(filename)=>{  }) {
-    if(dirPath.substr(-1)!=='/') dirPath+='/';
-    let fileList=getFileList(dirPath);
-    processStartedCallback(fileList);
+function getFilesHashFromFileList(fileList, processingCallback=(filename)=>{  }) {
     let filesHash={ };
-    fileList.forEach((filename)=>{
-        processingCallback(filename);
-        filesHash[filename]=getCommonHashFromFile(dirPath+filename);
+    fileList.forEach((file)=>{
+        processingCallback(file.path+file.name);
+        filesHash[file.name]=getCommonHashFromFile(file.path+file.name);
     })
     return filesHash;
 }
 
+program
+    .name("getHash")
+    .description("Get hash from files or all the files in dirs.")
+    .version("0.1")
+;
+program
+    .command("gen")
+    .description("Path of files or dirs")
+    .argument("<string>","Path or paths, if there are multiple files, use the separator you set(',' for default) to split the paths.")
+    .option("-s,--separator <char>", "separator character", ',')
+    .option("-j,--json","output json")
+    .option("-o,--output <string>", "the path to output result (NOTE: if this option is set, the json option will be set automatically.)")
+    .option("-d,--dir <string>","set a default dir for files")
+    .action((arg, options)=>{
+        let paths=arg.split(options.separator);
+        let fileList=[];
+        if(options.dir) {
+            try {
+                process.chdir(options.dir);
+            }
+            catch (err) {
+                console.warn(err.toString());
+            }
+        }
+        paths.forEach((path)=>{
+            let fileStat=fs.statSync(path);
+            if(fileStat.isDirectory()) {
+                fileList=fileList.concat(getFileListFromDir(path));
+            }
+            if(fileStat.isFile()) {
+                let filename=path.split("/").pop();
+                let filepath=path.substr(0,path.length-filename.length);
+                fileList.push({path: filepath, name: filename});
+            }
+        });
+        console.log("Generating common hash to "+fileList.length+" files...");
+        let hashData=getFilesHashFromFileList(fileList,
+            (filename)=>{
+                console.log("Processing "+filename+" ...");
+            }
+        );
+        console.log("Done!");
+        if(options.output) {
+            fs.writeFileSync(options.output, JSON.stringify(hashData));
+        }
+        else {
+            if(options.json) console.log(JSON.stringify(hashData));
+            else console.log(hashData);
+        }
+    })
+;
 
-// TODO
-console.log(getFilesHashFromDir(".",(fileList)=>{
-    console.log("Generating common hash to "+fileList.length+" files...");
-},(filename)=>{
-    console.log("Processing "+filename+" ...");
-}));
+program.parse();
