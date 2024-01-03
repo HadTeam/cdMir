6a63235e141f6e5a5df7bbdeb1661c24686ec9d7
CornWorld <cw@had.ink>
2024-01-04 01:11:56 +0800

    [Init] Make the package config more perfect

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,23 @@
 export {};
 
 declare global {
-	interface
+	interface HashValue {
+		md5?: string;
+		sha1?: string;
+		sha256?: string;
+	}
+
+	interface DownloadParam {
+		url: string;
+		filename: string;
+		headers?: string[];
+		cookies?: string[];
+		referrer?: string;
+		userAgent?: string;
+		hash?: HashValue;
+	}
+
+	interface Aria2Params {
+
+	}
 }
